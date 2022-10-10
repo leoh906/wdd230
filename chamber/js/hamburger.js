0a95de93e10fc266261bc58f5eaf7be7813dbfb8
Leo Harrison <leo.harrison96@gmail.com>
2022-10-10 09:14:57 -0600

    create toggle menu

--- chamber/js/hamburger.js
+++ chamber/js/hamburger.js
@@ -1,9 +1,16 @@
-let navElement = document.getElementById('')
+let navElement = document.getElementById('hamburger_button')
+let navMenu = document.getElementById('')
 
 
 navElement.addEventListener('click',toggleMenu);
 
 
 function toggleMenu() {
-
+    let toggleElement = document.getElementById('hamburger_content');
+    if (toggleElement.style.display === "block") {
+      toggleElement.style.display = "none";
+    } else {
+      toggleElement.style.display = "block";
+      toggleElement.style.textAlign = "center";
+    }
 }
