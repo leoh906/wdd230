b0e1cef0926915deccd3987a76fb21e094621cff
Leo Harrison <leo.harrison96@gmail.com>
2022-10-17 09:51:24 -0600

    Add basic input in each div

--- chamber/js/hamburger.js
+++ chamber/js/hamburger.js
@@ -8,6 +8,7 @@ function toggleMenu() {
     let toggleElement = document.getElementById('hamburger_content');
     if (toggleElement.style.display === "block") {
       toggleElement.style.display = "none";
+      toggleElement.style.textAlign = "center";
     } else {
       toggleElement.style.display = "block";
       toggleElement.style.textAlign = "center";
