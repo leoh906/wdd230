baea9aaf27ccfdc67d9f3feaadc6002fc8414cb3
Leo Harrison <leo.harrison96@gmail.com>
2022-10-31 10:14:46 -0600

    fix javascript

--- chamber/js/lastvisited.js
+++ chamber/js/lastvisited.js
@@ -1,5 +1,23 @@
-let lastVisited = Date.lastVisited;
-let current = new Date();
-const theElement = document.getElementById('discover_page_date')
-// let daysSince = XMLDocument
-theElement.innerHTML = current - lastVisited;
+
+let documentItem = document.getElementById('discover_page_date')
+
+if(localStorage.getItem('lastVisit')) {
+    // Get value from Local Storage
+    let previousItem = localStorage.getItem('lastVisit')
+    // Get Todays Date
+    let thisDate = Date.now(); 
+    // Subtract the variables
+    let subtractValue = thisDate - previousItem;
+    let editSubtract = subtractValue/1000/60/60/24;
+    documentItem.innerHTML = Math.round(editSubtract);
+
+
+}
+
+else {
+    localStorage.setItem('lastVisit',Date.now());
+    let todaysDate = localStorage.getItem('lastVisit');
+    documentItem.innerHTML = todaysDate;
+
+}
+
