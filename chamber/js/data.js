368b4052458b95eae4bf8e1a7275cc063a625220
Leo Harrison <leo.harrison96@gmail.com>
2022-11-14 09:44:46 -0700

    push image

--- chamber/js/data.js
+++ chamber/js/data.js
@@ -3,7 +3,7 @@
     // 1. Create a fetch, to get data from a previously made JSON File 
 console.log('Hello World')
 
-getURL = 'https://leoh906.github.io/business_json_file/data.json'
+getURL = 'https://leoh906.github.io/wdd230/chamber/data.json'
 
 // Creates a function to get the items of the JSON File  
 async function getItems(getURL) {
@@ -75,9 +75,49 @@ tableButton.addEventListener("click",showTables);
     // Create a function to display the businesses in card view
     function showCards() {
             console.log('This Function Works')
-            generalListElement.classList.remove('business_lists')
-            generalListElement.classList.add('business_cards')
+            // generalListElement.classList.remove('business_lists')
+            // generalListElement.classList.add('business_cards')
+            let tableRow = querySelectorAll('.business_cards')
+            tableRow.forEach((item) => {
+                item.remove();
+            })
+
+            businessDirectory.forEach(element => {
+                let divCard = document.createElement('div')
+                divCard.setAttribute('class','business_card')
+                let itemURL = document.createElement('a')
+                let itemImage = document.createElement('img')
+                let itemName = document.createElement('h1')
+                let itemAddress = document.createElement('p')
+                let itemTelephone = document.createElement('p')
+               
+            
+                generalListElement.appendChild(divCard)
+                divCard.appendChild(itemImage)
+                divCard.appendChild(itemName)
+                divCard.appendChild(itemAddress)
+                divCard.appendChild(itemTelephone)
+                divCard.appendChild(itemURL)
+                
+                itemImage.setAttribute('src',element.img)
+                itemImage.setAttribute('alt',"Alternative Text for the Image")
+                itemName.innerHTML = element.name;
+                itemAddress.innerHTML = element.address;
+                itemTelephone.innerHTML = element.telephone;
+                itemURL.setAttribute('href',element.URL);
+                itemURL.setAttribute('target','_blank');
+                itemURL.innerHTML = element.URL;
+                
+            }); 
+
+            businessDirectory.forEach(element => {
+                
+            })
+            
+            
+         
            
+
        
         }
 
@@ -94,19 +134,21 @@ tableButton.addEventListener("click",showTables);
          // Create a function to display the businesses in table view
          function showTables() {
             console.log('This Function also works')
-            generalListElement.classList.remove('business_cards')
-            generalListElement.classList.add('business_lists')
-            let divCard = document.createElement('div')
+            // generalListElement.classList.remove('business_cards')
+            // generalListElement.classList.add('business_lists')
+            let cardElements = document.querySelectorAll('.business_lists')
+            cardElements.forEach((item) => {
+                item.remove();
+            })
 
-            divCard.removeChild(itemImage)
-            divCard.removeChild(itemName)
-            divCard.removeChild(itemAddress)
-            divCard.removeChild(itemTelephone)
-            divCard.removeChild(itemURL)
-            generalListElement.removeChild(divCard)
+
+
+
+          
            
 
             // businessDirectory.forEach(element => {
+
 
             // })
 
@@ -123,7 +165,7 @@ tableButton.addEventListener("click",showTables);
 
                        
 
-            });
+            
             
     
         }
