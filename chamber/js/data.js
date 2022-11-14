a5d76efc9f3706ca0ee5f6fcafdb2e9a8a8e3bcf
Leo Harrison <leo.harrison96@gmail.com>
2022-11-14 06:52:11 -0700

    Add native JSON file

--- chamber/js/data.js
+++ chamber/js/data.js
@@ -7,91 +7,133 @@ getURL = 'https://leoh906.github.io/business_json_file/data.json'
 
 // Creates a function to get the items of the JSON File  
 async function getItems(getURL) {
+    let generalListElement = document.querySelector('.business_cards')
+    let divCard = document.createElement('div')
+    divCard.setAttribute('class','business_card')
+    let itemURL = document.createElement('a')
+    let itemImage = document.createElement('img')
+    let itemName = document.createElement('h1')
+    let itemAddress = document.createElement('p')
+    let itemTelephone = document.createElement('p')
     const response = await fetch(getURL);
     console.log(response);
-    // Always include this response
-    if(response.ok) {
-        // Do all the stuff with JavaScript
-        const jsObject = await response.json()
-        console.log(jsObject)
-        const businessDirectory = jsObject['companies']
-        // Create two event listeners for each button that was created
-        let cardButton = document.getElementById('card_view_button')
-        let tableButton = document.getElementById('list_view_button')
-        // let cardList = document.querySelector('.card_list')
+    
+    
+    
+    
+    
+// Always include this response
+if(response.ok) {
+    // Do all the stuff with JavaScript
+    const jsObject = await response.json()
+    console.log(jsObject)
+    const businessDirectory = jsObject['companies']
+    // Create two event listeners for each button that was created
+    let cardButton = document.getElementById('card_view_button')
+    let tableButton = document.getElementById('list_view_button')
+
+// Fetch the json data to show on startup
+businessDirectory.forEach(element => {
+    let divCard = document.createElement('div')
+    divCard.setAttribute('class','business_card')
+    let itemURL = document.createElement('a')
+    let itemImage = document.createElement('img')
+    let itemName = document.createElement('h1')
+    let itemAddress = document.createElement('p')
+    let itemTelephone = document.createElement('p')
+   
+
+    generalListElement.appendChild(divCard)
+    divCard.appendChild(itemImage)
+    divCard.appendChild(itemName)
+    divCard.appendChild(itemAddress)
+    divCard.appendChild(itemTelephone)
+    divCard.appendChild(itemURL)
+    
+    itemImage.setAttribute('src',element.img)
+    itemImage.setAttribute('alt',"Alternative Text for the Image")
+    itemName.innerHTML = element.name;
+    itemAddress.innerHTML = element.address;
+    itemTelephone.innerHTML = element.telephone;
+    itemURL.setAttribute('href',element.URL);
+    itemURL.setAttribute('target','_blank');
+    itemURL.innerHTML = element.URL;
+    
+}); 
+
+
+
+
+
+
 
 cardButton.addEventListener("click",showCards);
 tableButton.addEventListener("click",showTables);
 
+
+
     // Create a function to display the businesses in card view
     function showCards() {
-        console.log('This Function Works')   
-        businessDirectory.forEach(element => {
+            console.log('This Function Works')
+            generalListElement.classList.remove('business_lists')
+            generalListElement.classList.add('business_cards')
+           
+       
+        }
 
-            let divElement = document.querySelector('.card_list')
-            let divCard = document.createElement('div')
-            divCard.setAttribute('id','business_card')
-            let itemName = document.createElement('h1')
-            let itemAddress = document.createElement('p')
-            let itemTelephone = document.createElement('p')
-            let itemURL = document.createElement('a')
-            let itemImage = document.createElement('img')
-    
-            divElement.appendChild(divCard)
-            divElement.appendChild(itemImage)
-            divCard.appendChild(itemName)
-            divCard.appendChild(itemAddress)
-            divCard.appendChild(itemTelephone)
-            divCard.appendChild(itemURL)
-            
-            itemImage.setAttribute('src',element.img)
-            itemImage.setAttribute('alt',"Alternative Text for the Image")
-            itemName.innerHTML = element.name;
-            itemAddress.innerHTML = element.address;
-            itemTelephone.innerHTML = element.telephone;
-            itemURL.innerHTML = element.URL;
-            
-        });
+      
 
-        } // Ends the show cards function 
+
+
+
+
+
+ 
+   
     
          // Create a function to display the businesses in table view
          function showTables() {
             console.log('This Function also works')
-            businessDirectory.forEach(element => {
-            let divObject = document.querySelector('.table_list')
-            let divTable = document.createElement('table')
-            let divRow = document.createElement('tr')
-            let divItem = document.createElement('td')
-            let tableName = document.createElement('p')
-            let tableAddress = document.createElement('p')
-            let tablePhone = document.createElement('p')
-            let tableSite = document.createElement('p')
+            generalListElement.classList.remove('business_cards')
+            generalListElement.classList.add('business_lists')
+            let divCard = document.createElement('div')
 
-            divObject.appendChild(divTable)
-            divTable.appendChild(divRow)
-            divRow.appendChild(divItem)
-            divItem.appendChild(tableName)
-            divItem.appendChild(tableAddress)
-            divItem.appendChild(tablePhone)
-            divItem.appendChild(tableSite)
+            divCard.removeChild(itemImage)
+            divCard.removeChild(itemName)
+            divCard.removeChild(itemAddress)
+            divCard.removeChild(itemTelephone)
+            divCard.removeChild(itemURL)
+            generalListElement.removeChild(divCard)
+           
 
-            tableName.innerHTML = element.name
-            tableAddress.innerHTML = element.address
-            tablePhone.innerHTML = element.telephone
-            tableSite.innerHTML = element.URL            
+            // businessDirectory.forEach(element => {
+
+            // })
+
+            
+                
+          
+
+
+
+
+
+
+
+
+                       
 
             });
             
     
         }
+    } // Ends the if(ok) thingy
     
    
 
 
 
-
-    }
+    
 }; // This ends the large async function
 
 // We call the large async function
